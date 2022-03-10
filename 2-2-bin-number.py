4ba121ea15ec0eb4e05d7f4794627528900b46bd
AigulBulatova <aygul.bulatova.04@mail.ru>
2022-03-10 18:37:23 +0300

    add scripts

---
+++ 2-2-bin-number.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+number = []
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(dac, GPIO.OUT)
+
+number = [0, 0, 0, 0, 0, 0, 0, 1]
+
+GPIO.output (dac, number)
+
+time.sleep(15)
+
+GPIO.output (dac, 0)
+
+GPIO.cleanup() 
